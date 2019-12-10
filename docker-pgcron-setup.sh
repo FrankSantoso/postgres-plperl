ce461f84b27101b287d7332f39f290d934dbe5e7
Alex Frank Adhyatma <alex@solusi.tech>
2019-12-10 14:04:52 +0700

    added cron database

--- docker-pgcron-setup.sh
+++ docker-pgcron-setup.sh
@@ -3,6 +3,7 @@
 cat <<EOT >> /var/lib/postgresql/data/postgresql.conf
 shared_preload_libraries='pg_cron'
 cron.database_name='${POSTGRES_DB:-postgres}'
+cron.database_name=uripstok19
 EOT
 
 # Required to load pg_cron
